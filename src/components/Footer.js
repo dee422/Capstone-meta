e7e098a0f7ee0b7e0cc113ab4f933b0e1e2dcc08
Edward Dee <804038949@qq.com>
2025-07-19 22:35:07 +0800

    Semantic structure

---
+++ src/components/Footer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <div>
+        <h3>Little Lemon</h3>
+        <p>About text from wireframe...</p>
+      </div>
+      <div>
+        <h4>Doormat Navigation</h4>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Menu</li>
+          <li>Reservations</li>
+          <li>Order Online</li>
+          <li>Login</li>
+        </ul>
+      </div>
+      <div>
+        <h4>Contact</h4>
+        <p>Address<br />Phone<br />Email</p>
+      </div>
+      <div>
+        <h4>Social Media</h4>
+        <p>Instagram<br />Facebook<br />Twitter</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
